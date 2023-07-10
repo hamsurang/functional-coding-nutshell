d293c9dff3e52b9f6ab4b24b176ec11b568f2dcd
minsoo kim <zlemzlem5656@naver.com>
2023-07-11 01:26:42 +0900

    chore: 불필요한 설정 제거

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -5,15 +5,6 @@ const nextra = require("nextra");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@impack/foundation", "@impack/ui"],
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
 };
 
 /** @type {import('nextra').NextraConfig} */
