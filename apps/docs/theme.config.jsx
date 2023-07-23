f84af5be65f3ada7d3b4721813cc123002a66e3d
minsoo-web <zlemzlem5656@naver.com>
2023-07-23 16:06:52 +0900

    chore: add og meta info

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,3 +1,5 @@
+import OgImage from './public/og.png'
+
 /** @type {import('nextra-theme-docs').DocsThemeConfig} */
 export default {
   logo: <span>쏙쏙쑥쑥</span>,
@@ -12,8 +14,17 @@ export default {
   useNextSeoProps() {
     return {
       titleTemplate: '%s – 쏙쏙쑥쑥',
-    };
+    }
   },
+  head: () => (
+    <>
+      <meta
+        property="og:description"
+        content="쏙쏙쑥쑥 스터디 - 함수랑 산악회 주최"
+      />
+      <meta property="og:image" content={OgImage.src} />
+    </>
+  ),
   footer: {
     text: () => (
       <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -29,4 +40,4 @@ export default {
       </div>
     ),
   },
-};
+}
