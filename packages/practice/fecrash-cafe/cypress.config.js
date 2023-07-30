881196d4815dab3468d667f9dc7ef9432d3472e3
minsoo kim <zlemzlem5656@naver.com>
2023-07-30 21:58:59 +0900

    chore: 예제 올리기, README.md 파일 추가

--- packages/practice/fecrash-cafe/cypress.config.js
+++ packages/practice/fecrash-cafe/cypress.config.js
@@ -1,8 +1,9 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     screenshotOnRunFailure: false,
     video: false,
+    specPattern: ['./**/*.cy.{js,jsx,ts,tsx}'],
   },
-});
+})
