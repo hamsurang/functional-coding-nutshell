b6dfaafe6503d561e40996b9f20b0c6e10d4a50f
minsoo kim <zlemzlem5656@naver.com>
2023-08-02 12:15:08 +0900

    practice: 1주차 3번 문제 제출

--- packages/example/src/1week/03/__submit__/minsoo-web.test.ts
+++ packages/example/src/1week/03/__submit__/minsoo-web.test.ts
@@ -8,6 +8,20 @@ describe('multiDimensionalAccumulate', () => {
       [11, 12, 13, 14, 15, 16, 17, 18, 19],
       [19, 18, 17, 16, 15, 14, 13, 12, 11],
     ]
-    expect(multiDimensionalAccumulate(multiDimensionalArr)).toBe(86)
+
+    expect(multiDimensionalAccumulate(multiDimensionalArr)).toBe(360)
+    expect(multiDimensionalAccumulate(multiDimensionalArr)).toBe(360)
+  })
+
+  it('case: 2', () => {
+    const multiDimensionalArr = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [9, 8, 7, 6, 5, 4, 3, 2, 1],
+      [11, 12, 13, 14, 15, 16, 17, 18, 19],
+      [19, 18, 17, 16, 15, 14, 13, 12, 11],
+    ]
+
+    expect(multiDimensionalAccumulate(multiDimensionalArr, 10)).toBe(370)
+    expect(multiDimensionalAccumulate(multiDimensionalArr, 10)).toBe(370)
   })
 })
