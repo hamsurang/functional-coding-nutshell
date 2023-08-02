16ee0634b41aa8c967f46b0d094578d8408c11dc
minsoo kim <zlemzlem5656@naver.com>
2023-08-02 11:52:14 +0900

    practice: 1주차 2번 문제 제출

--- packages/example/src/1week/02/__submit__/minsoo-web.test.ts
+++ packages/example/src/1week/02/__submit__/minsoo-web.test.ts
@@ -2,6 +2,23 @@ import { accumulate } from './minsoo-web'
 
 describe('accumulate', () => {
   it('case: 1', () => {
-    expect(accumulate([1, 2, 3, 4, 5])).toBe(15)
+    const foo = [1, 2, 3, 4, 5]
+
+    expect(accumulate(foo)).toBe(15)
+    expect(accumulate(foo)).toBe(15)
+  })
+
+  it('case: 2', () => {
+    const foo = [1, 2, 3, 4, 5]
+
+    expect(accumulate(foo, 10)).toBe(25)
+    expect(accumulate(foo, 10)).toBe(25)
+  })
+
+  it('case: 3', () => {
+    const foo = [-1, 2, -3, 4, 5]
+
+    expect(accumulate(foo)).toBe(7)
+    expect(accumulate(foo)).toBe(7)
   })
 })
