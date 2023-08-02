3d3984cdcaaa2b28341f97fd971c935087bbc365
minsoo kim <zlemzlem5656@naver.com>
2023-08-02 12:19:16 +0900

    잘못 올린 push 원복

--- packages/example/src/1week/03/__submit__/minsoo-web.ts
+++ packages/example/src/1week/03/__submit__/minsoo-web.ts
@@ -1,16 +1,15 @@
-const sum = (a: number, b: number): number => a + b
+export function multiDimensionalAccumulate(
+  multiDimensionalArr: number[][],
+): number {
+  let accumulator = 0
 
-/**
- * 접근방법:
- * flat과 reduce를 조합해서 간단하게 바꿔봤습니다.
- * 만약, 개선된 2번 문제를 사용할 수 있었다면, reduce의 콜백함수로 활용해서, flat 없이 작업해볼 수 있겠다? 라는 생각도 해봤습니다.
- *
- * @param arr 2중 배열입니다.
- * @returns 배열의 모든 숫자들의 합이 계산됩니다.
- */
-export const multiDimensionalAccumulate = (
-  arr: number[][],
-  initialValue = 0,
-): number => {
-  return arr.flat().reduce(sum, initialValue)
+  for (let i = 0; i < multiDimensionalArr.length; i += 1) {
+    for (let j = 0; j < multiDimensionalArr[i].length; j += 1) {
+      if (j < i) {
+        accumulator += multiDimensionalArr[i][j]
+      }
+    }
+  }
+
+  return accumulator
 }
