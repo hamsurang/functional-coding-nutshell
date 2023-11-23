bd3b9f592f69292d8dd540a579dcac2d6dfec854
999 <999@hyeon.pro>
2023-11-23 22:47:48 +0900

    hotfix 함수 자신이 정의할수있도록 변경

--- packages/example/src/2week/compose/compose.spec.ts
+++ packages/example/src/2week/compose/compose.spec.ts
@@ -2,12 +2,8 @@ import { compose } from './compose'
 
 describe('pop', () => {
   it('더하기 함수와 곱하기합수를 합성했습니다.', () => {
-    function add(a: number, b: number) {
-      return a + b
-    }
-    function multiply(a: number, b: number) {
-      return a * b
-    }
+    // add함수!
+    // multiply함수!
     const result = compose(4, add(7), multiply(5))
     assert(result).toBe(55)
   })
